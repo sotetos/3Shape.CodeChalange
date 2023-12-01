d694e7109332e377e6d77a17f948ba499e7ddfa2
Astrid Weiss <pikminj@gmail.com>
2023-11-30 23:19:18 -0500

    Add Video Types

---
+++ src/3Shape.CodeChallange/Models/Video/VideoItemBase.cs
@@ -0,0 +1,19 @@
+﻿using Models.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Video
+{
+    public abstract class VideoItemBase: LibraryItemBase
+    {
+        public List<VideoTrack> Tracks { get; set; } = new List<VideoTrack>();
+        public virtual IEnumerable<string> GetCreators()
+        {
+            return Tracks.Select(t => t.Artist).Distinct();
+        }
+
+    }
+}
