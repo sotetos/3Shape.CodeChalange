bebde025eb4b06d3aaa1466340556b20da973262
Astrid Weiss <pikminj@gmail.com>
2023-12-01 00:19:42 -0500

    Add Parsing models

--- src/3Shape.CodeChallange/Services/Internals/ImportDataParser.cs
+++ src/3Shape.CodeChallange/Services/Internals/ImportDataParser.cs
@@ -1,8 +1,13 @@
-﻿using Services.Ports;
+﻿using Models;
+using Services.Internals.Models;
+using Services.Ports;
 
 namespace Services.Internals
 {
     internal class ImportDataParser : IImportDataParser
     {
+        public IEnumerable<ParsedInputData> ParseInput(string inputString)
+        {
+        }
     }
 }
