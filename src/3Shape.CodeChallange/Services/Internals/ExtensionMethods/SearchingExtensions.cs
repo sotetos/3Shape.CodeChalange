a372e5a503508fe79b1a54111541b5ebeb43ba6d
Astrid Weiss <pikminj@gmail.com>
2023-12-01 10:02:06 -0500

    Clean up usings

--- src/3Shape.CodeChallange/Services/Internals/ExtensionMethods/SearchingExtensions.cs
+++ src/3Shape.CodeChallange/Services/Internals/ExtensionMethods/SearchingExtensions.cs
@@ -1,10 +1,5 @@
 ﻿using Models;
 using Models.Text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Services.Internals.Models;
 
 namespace Services.Internals.ExtensionMethods
--- src/3Shape.CodeChallange/Services/Internals/Models/ParsedSearchCondition.cs
+++ src/3Shape.CodeChallange/Services/Internals/Models/ParsedSearchCondition.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Services.Internals.Models
+﻿namespace Services.Internals.Models
 {
     public record ParsedSearchCondition
     {
