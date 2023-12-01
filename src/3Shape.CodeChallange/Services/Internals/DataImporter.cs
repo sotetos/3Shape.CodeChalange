a73ceeaeb81e99054bbe68789841d5b67c1749d7
Astrid Weiss <pikminj@gmail.com>
2023-12-01 00:52:15 -0500

    Impliment ReadBooks

--- src/3Shape.CodeChallange/Services/Internals/DataImporter.cs
+++ src/3Shape.CodeChallange/Services/Internals/DataImporter.cs
@@ -1,4 +1,7 @@
-﻿using Models.Text;
+﻿using Models;
+using Models.Text;
+using Services.Internals.ExtensionMethods;
+using Services.Internals.Models;
 using Services.Ports;
 
 namespace Services.Internals
@@ -18,12 +21,47 @@ namespace Services.Internals
 
         public List<Book> ReadBooks(string input)
         {
-            return new List<Book>(1);
+            var data = _importDataParser.ParseInput(input);
+
+            var results = data
+                .Where(d => !d.HasErrors)
+                .Select(BuildLibraryItem);
+
+            return results
+                .Where(r => r.LibraryItemType == LibraryItemType.Book)
+                .Select(r => (Book)r)
+                .ToList();
         }
 
         public List<Book> FindBooks(string searchString)
         {
             return new List<Book>(1);
+        }
+
+        private static LibraryItemBase BuildLibraryItem(ParsedInputData data) => data.InputType switch
+        {
+            LibraryItemType.Book => BuildBook(data),
+            LibraryItemType.CD => null,
+            LibraryItemType.DVD => null,
+            LibraryItemType.BlueRay => null,
+            LibraryItemType.EBook => null,
+            LibraryItemType.AudioFile => null,
+            LibraryItemType.VideoFile => null,
+            LibraryItemType.Other => null,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        private static Book BuildBook(ParsedInputData data)
+        {
+            var result = new Book()
+            {
+                Id = Guid.NewGuid()
+            };
+            foreach (var propertyValuePair in data.PropertyValueData)
+            {
+                result.ParsePropertyValuePair(propertyValuePair);
+            }
+            return result;
         }
     }
 }
--- src/3Shape.CodeChallange/Services/Ports/IDataImporter.cs
+++ src/3Shape.CodeChallange/Services/Ports/IDataImporter.cs
@@ -1,6 +1,8 @@
-﻿namespace Services.Ports;
+﻿using Models.Text;
+
+namespace Services.Ports;
 
 public interface IDataImporter
 {
-    
+    List<Book> ReadBooks(string input);
 }
